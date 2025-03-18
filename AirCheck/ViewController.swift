96d086979338c41f542ff24db0d5f77114f30cba
Akylbek <akylbek.oralov@nu.edu.kz>
2025-03-18 22:33:59 +0500

    Initial Commit

---
+++ AirCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirCheck
+//
+//  Created by Akylbek Oralov on 18.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
