126f7b37bf7ab644a0922f181be58ea8fa71f820
Akylbek <akylbek.oralov@nu.edu.kz>
2025-03-19 00:01:31 +0500

    MapBox Setted Up

--- AirCheck/ViewController.swift
+++ AirCheck/ViewController.swift
@@ -7,24 +7,26 @@
 
 import UIKit
 import SnapKit
+import MapboxMaps
 
 class ViewController: UIViewController {
-
+    
+    var mapView: MapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .white
+        mapView = MapView(frame: view.bounds)
+        let cameraOptions = CameraOptions(center:
+                                            CLLocationCoordinate2D(latitude: 39.5, longitude: -98.0),
+                                          zoom: 2, bearing: 0, pitch: 0)
+        mapView.mapboxMap.setCamera(to: cameraOptions)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         
-        let text = UILabel()
-        text.text = "Hello, World!"
-        view.addSubview(text)
-        
-        text.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-        }
+        view.addSubview(mapView)
     }
-
-
+    
+    
 }
 
